23cbe4c66c8f2cb09629f7e35e588bec82f8a491
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2025-10-13 10:23:46 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (41.41%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        set<int> s(nums.begin(), nums.end());  // store unique + sorted
+        long long first = LLONG_MIN, second = LLONG_MIN, third = LLONG_MIN;
+        bool thirdExists = false;
 
-        // If less than 3 distinct numbers, return the largest
-        if (s.size() < 3)
-            return *s.rbegin();
+        for (int num : nums) {
+            if (num == first || num == second || num == third) continue;
 
-        // Move iterator 2 steps ahead from rbegin() → third largest
-        auto it = s.rbegin();
-        advance(it, 2);
-        return *it;
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if (num > second) {
+                third = second;
+                second = num;
+            } else if (num > third) {
+                third = num;
+            }
+        }
+
+        return (third == LLONG_MIN) ? first : third;
     }
 };

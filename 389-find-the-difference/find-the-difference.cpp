746b3aa75a2831476f14df4f0adeeba432a151bf
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2025-10-14 11:51:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (52.30%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        map<char,int>mp;
-        char c;
-        for(auto i:s){
-            mp[i]++;
-        }
-        for(auto i:t){
-            mp[i]++;
-        }
-        for (auto it = mp.begin(); it != mp.end(); ++it) {
-           if(it->second%2!=0){
-                return it->first;
-           }
-        }
-        return '\0';
-
+        char res = 0;
+        for (char ch : s) res ^= ch;  // XOR all chars in s
+        for (char ch : t) res ^= ch;  // XOR all chars in t
+        return res;                   // remaining char is the extra one
     }
 };

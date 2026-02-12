f22da1856c2ce5c88db2ceb73b28ac4f5d75503b
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-02-12 07:43:49 +0530

    Time: 11 ms (69.32%) | Memory: 50.3 MB (54.74%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,34 +1,38 @@
 class Solution {
-    
-    public int shipWithinDays(int[] weights, int days) {
-        int mini=weights[0];
-        int wholesum=0;
-        for(int i:weights){
-            mini=Math.max(i,mini);
-            wholesum+=i;
-        }
-        int maxi=wholesum;
-        int miniday=0;
-        while(mini<=maxi){
-            int c=1;
-            int sum=0;
-            int mid=mini+(maxi-mini)/2;
-            for(int i:weights){
-                if(sum+i>mid){
-                    sum=0;
-                    c++;
-                }
-                sum+=i;
+    public int canship(int[] w,int mid){
+        int days=0;
+        int sum=0;
+        for(int i:w){
+            if(sum+i>mid){
+                sum=0;
+                days++;
             }
-            if(c<=days){
-                maxi=mid-1;
-                miniday=mid;
+            sum+=i;
+        }
+        days++;
+        return days;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int n=weights.length;
+        int low=1;
+        int high=0;
+        for(int i:weights){
+            low=Math.max(low,i);
+            high+=i;
+        }
+
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(canship(weights,mid)<=days){
+                ans=mid;
+                high=mid-1;
             }
             else{
-                mini=mid+1;
+                low=mid+1;
             }
-
         }
-        return miniday; 
+        return ans;
+
     }
 }

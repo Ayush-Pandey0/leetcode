3cda4e238c3f2524389b232e4c60d8b32d25473a
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-02-13 21:54:03 +0530

    Time: 805 ms (9.82%) | Memory: 81.1 MB (83.27%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,7 +1,51 @@
 class Solution {
+    public void sorting(int[] nums,int low,int mid,int high){
+        int[] temp=new int[high+1];
+
+        int i=low;
+        int j=mid+1;
+
+        int count=0;
+        while(i<=mid&&j<=high){
+            if(nums[i]<nums[j]){
+                temp[count]=nums[i];
+                i++;
+            }
+            else{
+                temp[count]=nums[j];
+                j++;
+            }
+            count++;
+        }
+        while(i<=mid){
+            temp[count]=nums[i];
+            i++;
+            count++;
+        }
+        while(j<=high){
+            temp[count]=nums[j];
+            j++;
+            count++;
+        }
+
+
+        for(int m=low;m<=high;m++){
+            nums[m]=temp[m-low];
+        }
+    }
+
+    public void merges(int[] nums, int low, int high) {
+        if (low == high)
+            return;
+        int mid = low + (high - low) / 2;
+        merges(nums, low, mid);
+        merges(nums, mid + 1, high);
+
+        sorting(nums, low, mid, high);
+    }
+
     public int[] sortArray(int[] nums) {
-        int n=nums.length;
-        Arrays.sort(nums);
+        merges(nums, 0, nums.length - 1);
         return nums;
     }
 }

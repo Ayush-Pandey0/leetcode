7c4eda5519669ab10770bdde3006fb64590f3385
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-02-13 12:46:04 +0530

    Time: 21 ms (90.19%) | Memory: 83.4 MB (48.87%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,15 +1,7 @@
 class Solution {
     public int[] sortArray(int[] nums) {
         int n=nums.length;
-        for(int i=1;i<n;i++){
-            int j=i-1;
-            int key=nums[i];
-            while(j>=0&& nums[j]>key){
-                nums[j+1]=nums[j];
-                j--;
-            }
-            nums[j+1]=key;
-        }
+        Arrays.sort(nums);
         return nums;
     }
 }

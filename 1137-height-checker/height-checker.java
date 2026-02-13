ff6353562bd7ede13162180fcb18450a9c03356c
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-02-13 09:45:21 +0530

    Time: 2 ms (88.18%) | Memory: 43.2 MB (90.97%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,13 +1,15 @@
 class Solution {
     public void sortb(int[] heights){
-        for(int i=0;i<heights.length-1;i++){
-            for(int j=0;j<heights.length-i-1;j++){
-                if(heights[j]>heights[j+1]){
-                    int temp=heights[j+1];
-                    heights[j+1]=heights[j];
-                    heights[j]=temp;
+        for(int i=0;i<heights.length;i++){
+            int smallest=i;
+            for(int j=i;j<heights.length;j++){
+                if(heights[j]<heights[smallest]){
+                    smallest=j;
                 }
             }
+            int temp=heights[smallest];
+            heights[smallest]=heights[i];
+            heights[i]=temp;
         }
     }
     public int heightChecker(int[] heights) {

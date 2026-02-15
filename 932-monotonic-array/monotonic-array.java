752c00e5f4719a0697d82089384c3aa1e6ac02fe
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-02-15 13:49:25 +0530

    Time: 1 ms (95.01%) | Memory: 85.5 MB (6.48%) - LeetSync

--- 932-monotonic-array/monotonic-array.java
+++ 932-monotonic-array/monotonic-array.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        boolean increasing = true;
-        boolean decreasing = true;
+        // if(nums.length < 2) return true;
+        boolean in = false;
+        boolean de = false;
 
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > nums[i - 1]) {
-                decreasing = false;
-            }
-            if (nums[i] < nums[i - 1]) {
-                increasing = false;
-            }
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i + 1] < nums[i]) de = true;
+            else if (nums[i + 1] > nums[i]) in = true;
+            else continue;
+
+            if(de && in) return false;
         }
 
-        return increasing || decreasing;
+        return true;
     }
 }

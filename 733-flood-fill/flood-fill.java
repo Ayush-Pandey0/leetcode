6bfe43cd78beece46ba5604f74ed2ae3e26b0242
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-04-20 13:28:34 +0530

    Time: 1 ms (38.47%) | Memory: 46.8 MB (64.85%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -6,24 +6,33 @@ class Solution {
         int faah=image[sr][sc];
         if(faah == color) return image;
         q.add(new int[]{sr,sc});
-        image[sr][sc]=color;
 
         int []dx={-1,1,0,0};
         int []dy={0,0,-1,1};
-        while(!q.isEmpty()){
-            int node[]=q.poll();
-            int a=node[0];
-            int b=node[1];
 
-            for(int i=0;i<4;i++){
-                int x=a+dx[i];
-                int y=b+dy[i];
-                if(x>=0&&y>=0&&x<n&&y<m&&image[x][y]==faah){
-                    image[x][y]=color;
-                    q.add(new int[]{x,y});
-                }
-            }
-        }
+        dfs(image,color,faah,sr,sc);
+        // while(!q.isEmpty()){
+        //     int node[]=q.poll();
+        //     int a=node[0];
+        //     int b=node[1];
+
+        //     for(int i=0;i<4;i++){
+        //         int x=a+dx[i];
+        //         int y=b+dy[i];
+        //         if(x>=0&&y>=0&&x<n&&y<m&&image[x][y]==faah){
+        //             image[x][y]=color;
+        //             q.add(new int[]{x,y});
+        //         }
+        //     }
+        // }
         return image;
+    }
+    void dfs(int [][]image,int color,int faah,int i,int j){
+        if(i<0||j<0||i>=image.length||j>=image[0].length||image[i][j]!=faah) return;
+        image[i][j]=color;
+        dfs(image,color,faah,i-1,j);
+        dfs(image,color,faah,i+1,j);
+        dfs(image,color,faah,i,j-1);
+        dfs(image,color,faah,i,j+1);
     }
 }

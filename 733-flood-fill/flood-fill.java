0c5f736f5fda6eeb99223cf22dcf93129cefe64a
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-04-20 13:15:33 +0530

    Time: 1 ms (38.47%) | Memory: 46.9 MB (27.88%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int n=image.length;
+        int m=image[0].length;
+        
+        Queue<int[]> q=new LinkedList<>();
+        int faah=image[sr][sc];
+        q.add(new int[]{sr,sc});
+        image[sr][sc]=color;
+
+        int []dx={-1,1,0,0};
+        int []dy={0,0,-1,1};
+        int c=m*n;
+        int count=0;
+        while(!q.isEmpty()&&count<c){
+            count++;
+            int node[]=q.poll();
+            int a=node[0];
+            int b=node[1];
+
+            for(int i=0;i<4;i++){
+                int x=a+dx[i];
+                int y=b+dy[i];
+                if(x>=0&&y>=0&&x<n&&y<m&&image[x][y]==faah){
+                    image[x][y]=color;
+                    q.add(new int[]{x,y});
+                }
+            }
+        }
+        return image;
+    }
+}

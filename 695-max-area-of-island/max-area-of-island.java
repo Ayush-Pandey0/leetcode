07e32a9b3339c677907850c945fafa8b8650aa7d
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-04-20 22:49:45 +0530

    Time: 2 ms (63.17%) | Memory: 46.4 MB (83.68%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -9,30 +9,41 @@ class Solution {
             for (int j = 0; j < m; j++) {
                 int count=0;
                 if (grid[i][j] == 1) {
-                    Queue<int[]> q = new LinkedList<>();
-                    q.add(new int[] { i, j });
-                    grid[i][j] = 0;
+                    count=dfs(i,j,grid);
+                    // Queue<int[]> q = new LinkedList<>();
+                    // q.add(new int[] { i, j });
+                    // grid[i][j] = 0;
                     
 
-                    while (!q.isEmpty()) {
-                        int []node=q.poll();
-                        int a=node[0];
-                        int b=node[1];
+                    // while (!q.isEmpty()) {
+                    //     int []node=q.poll();
+                    //     int a=node[0];
+                    //     int b=node[1];
 
-                        for(int k=0;k<4;k++){
-                            int x=dx[k]+a;
-                            int y=dy[k]+b;
-                            if(x>=0&&y>=0&&x<n&&y<m&&grid[x][y]==1){
-                                q.add(new int[]{x,y});
-                                grid[x][y]=0;
-                            }
-                        }
-                        count++;
-                    }
+                    //     for(int k=0;k<4;k++){
+                    //         int x=dx[k]+a;
+                    //         int y=dy[k]+b;
+                    //         if(x>=0&&y>=0&&x<n&&y<m&&grid[x][y]==1){
+                    //             q.add(new int[]{x,y});
+                    //             grid[x][y]=0;
+                    //         }
+                    //     }
+                    //     count++;
+                    // }
                 }
                 ans=Math.max(count,ans);
             }
         }
         return ans;
+    }
+    int dfs(int i,int j,int[][] grid){
+        if(i>=grid.length||j>=grid[0].length||i<0||j<0||grid[i][j]==0) return 0;
+        int area=1;
+        grid[i][j]=0;
+        area+=dfs(i-1,j,grid);
+        area+=dfs(i+1,j,grid);
+        area+=dfs(i,j-1,grid);
+        area+=dfs(i,j+1,grid);
+        return area;
     }
 }

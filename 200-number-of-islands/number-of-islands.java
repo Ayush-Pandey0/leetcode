9a68bcd758ed1bf99067cea631c8adb3f588f95a
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-04-20 12:36:22 +0530

    Time: 2 ms (99.88%) | Memory: 52.6 MB (15.76%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,36 +2,28 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int dx[]={-1,1,0,0};
-        int dy[]={0,0,-1,1};
+
+        int[] dx={-1,1,0,0};
+        int[] dy={0,0,-1,1};
         int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    Queue<int[]> q=new LinkedList<>();
-                    q.add(new int[]{i,j});
-                    grid[i][j]='0';
-                    while(!q.isEmpty()){
-                        int []node=q.poll();
-                        int x=node[0];
-                        int y=node[1];
-
-                        for(int k=0;k<4;k++){
-                            int newx=dx[k]+x;
-                            int newy=dy[k]+y;
-                            if(newx>=0&&newy>=0&&newx<n&&newy<m&&grid[newx][newy]=='1'){
-                                q.add(new int[]{newx,newy});
-                                grid[newx][newy]='0';
-
-                            }
-                        }
-                    }
-
-
+                    dfs(grid,i,j);
                 }
             }
         }
         return count;
+    }
+
+    void dfs(char[][] grid, int i, int j) {
+        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]=='0') return;
+        grid[i][j]='0';
+
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
     }
 }

fd1b10a6e382567be419af70dd4d757e0fcfb9ba
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-04-21 11:40:40 +0530

    Time: 1 ms (100.00%) | Memory: 43.8 MB (63.78%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.java
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.java
@@ -1,23 +1,32 @@
 class Solution {
     public int furthestDistanceFromOrigin(String moves) {
-        int countl=0;
-        int countr=0;
-        for(char i:moves.toCharArray()){
-            if(i=='L') countl++;
-            else if(i=='R') countr++;
+        int L = 0, R = 0, blank = 0;
+        
+        for(char c : moves.toCharArray()){
+            if(c == 'L') L++;
+            else if(c == 'R') R++;
+            else blank++; // '_'
         }
-        char replacement=(countl>countr)?'L':'R';
+        
+        return Math.abs(L - R) + blank;
+        // int countl=0;
+        // int countr=0;
+        // for(char i:moves.toCharArray()){
+        //     if(i=='L') countl++;
+        //     else if(i=='R') countr++;
+        // }
+        // char replacement=(countl>countr)?'L':'R';
 
-        countl=0;
-        countr=0;
-        for(char i:moves.toCharArray()){
-            char c;
-            if(i=='_')  c=replacement;
-            else  c=i;
+        // countl=0;
+        // countr=0;
+        // for(char i:moves.toCharArray()){
+        //     char c='';
+        //     if(i=='_')  c=replacement;
+        //     else  c=i;
 
-            if(c=='L') countl++;
-            else if(c=='R') countr++;
-        }
-        return Math.abs(countl-countr);
+        //     if(c=='L') countl++;
+        //     else if(c=='R') countr++;
+        // }
+        // return Math.abs(countl-countr);
     }
 }

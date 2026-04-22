53be439742ebeea53444ad6b9169f095e70f2367
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-04-22 21:07:18 +0530

    Time: 143 ms (44.20%) | Memory: 291.2 MB (26.19%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,33 +1,32 @@
 class Solution {
-    boolean dfs(int source,int destination,ArrayList<ArrayList<Integer>> adj,int[] visited){
-        if(source==destination) return true;
-        visited[source]=1;
-        for(int i:adj.get(source)){
-            if(visited[i]==0){
-                if(dfs(i,destination,adj,visited)){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-
-        for(int []edg:edges){
-            int u=edg[0];
-            int v=edg[1];
+        for(int i=0;i<edges.length;i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
 
-        int[] visited=new int[n];
-
-        boolean ans=dfs(source,destination,adj,visited);
-        return ans;
-
+        int visited[]=new int[n];
+        for(int i=0;i<n;i++){
+            if(visited[i]==0){
+                if(dfs(source,destination,adj,visited)) return true;
+            }
+        }
+        return false;
+    }
+    boolean dfs(int source,int destination,ArrayList<ArrayList<Integer>> adj,int[] visited){
+        visited[source]=1;
+        if(source==destination) return true;
+        for(int j:adj.get(source)){
+            if(visited[j]==0){
+                if(dfs(j,destination,adj,visited)) return true;
+            }
+        }
+        return false;
     }
 }

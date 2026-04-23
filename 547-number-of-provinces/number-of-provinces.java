5a4f486ad1c85e604c16fd9842e9bdc1085eeba5
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-04-23 08:34:24 +0530

    Time: 2 ms (43.00%) | Memory: 47.1 MB (75.20%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,9 +1,8 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
-        int m=isConnected[0].length;
-        int visited[]=new int[n];
         int count=0;
+        int n=isConnected.length;
+        int visited[]=new int[n];
         for(int i=0;i<n;i++){
             if(visited[i]==0){
                 count++;
@@ -12,8 +11,8 @@ class Solution {
                 visited[i]=1;
                 while(!q.isEmpty()){
                     int node=q.poll();
-                    for(int j=0;j<n;j++){
-                        if(isConnected[node][j]==1&&visited[j]==0){
+                    for(int j=0;j<isConnected.length;j++){
+                        if(visited[j]==0&&isConnected[node][j]==1){
                             q.add(j);
                             visited[j]=1;
                         }
@@ -22,5 +21,5 @@ class Solution {
             }
         }
         return count;
-    } 
+    }
 }

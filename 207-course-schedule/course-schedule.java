606b6ae8efec964462e1c00caf4396896ac1c419
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-04-29 09:15:51 +0530

    Time: 4 ms (93.15%) | Memory: 47.1 MB (47.88%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,30 +1,32 @@
 class Solution {
-    boolean dfs(int i,int []visited,int[] pathvisited,ArrayList<ArrayList<Integer>> adj){
+    boolean dfs(int i,int[]visited,int[] pathvisited,ArrayList<ArrayList<Integer>> adj){
         visited[i]=1;
         pathvisited[i]=1;
 
-        for(int j:adj.get(i)){
-            if(visited[j]==0){
-                if(dfs(j,visited,pathvisited,adj)==true) return true;
-            }else if(pathvisited[j]==1) return true;
+        for(int nei:adj.get(i)){
+            if(visited[nei]==0){
+                if(dfs(nei,visited,pathvisited,adj)==true) return true;
+            }else if(pathvisited[nei]==1){
+                return true;
+            }
         }
         pathvisited[i]=0;
         return false;
     }
     public boolean canFinish(int n, int[][] p) {
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        int visited[]=new int[n];
+        int pathvisited[]=new int[n];
+
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
 
         for(int i=0;i<p.length;i++){
-            int u=p[i][0];
-            int v=p[i][1];
-            adj.get(u).add(v);
+            int v=p[i][0];
+            int u=p[i][1];
+            adj.get(v).add(u);
         }
-
-        int visited[]=new int[n];
-        int pathvisited[]=new int[n];
 
         for(int i=0;i<n;i++){
             if(visited[i]==0){

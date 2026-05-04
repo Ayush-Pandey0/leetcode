a68abda6e019fc1421bfd55d9779d748bfeba4e2
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-05-04 10:03:18 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (16.04%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,17 +1,29 @@
 class Solution {
+    void reverse(int[][] m, int row) {
+    int left = 0;
+    int right = m.length - 1;
+
+    while (left < right) {
+        int temp = m[row][left];
+        m[row][left] = m[row][right];
+        m[row][right] = temp;
+
+        left++;
+        right--;
+    }
+}
     public void rotate(int[][] matrix) {
         int n=matrix.length;
-        int [][] ans=new int[n][n];
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<n;j++){
-                ans[j][n-1-i]=matrix[i][j];
-            }
-        }
+
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                matrix[i][j]=ans[i][j];
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
-        
+        for(int k=0;k<n;k++){
+            reverse(matrix,k);
+        }
     }
 }

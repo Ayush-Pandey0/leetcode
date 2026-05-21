934bdffd633028dafa966ce5b331b7a5dd4f16d1
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-05-21 09:00:00 +0530

    Time: 1 ms (98.45%) | Memory: 47.3 MB (75.95%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -6,11 +6,8 @@ class Solution {
         }
         int left=0;
         for(int i=0;i<nums.length;i++){
-            if (left == sum - left - nums[i]) {
-                return i;
-            }
-            left += nums[i];
-
+            if(left+nums[i]==sum-left) return i;
+            left+=nums[i];
         }
         return -1;
     }

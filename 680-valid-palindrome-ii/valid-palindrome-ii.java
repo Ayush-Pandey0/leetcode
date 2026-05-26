54f506acd9d9b18cdb88e7eaf8905bc2ec743888
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-05-26 11:57:25 +0530

    Time: 4 ms (99.65%) | Memory: 48 MB (12.17%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,38 +1,31 @@
 class Solution {
     public boolean validPalindrome(String s) {
-
         int i = 0;
-        int j = s.length() - 1;
-
-        while(i < j) {
-
-            if(s.charAt(i) != s.charAt(j)) {
-
-                int l = i + 1;
-                int r = j;
-
-                while(l < r && s.charAt(l) == s.charAt(r)) {
-                    l++;
-                    r--;
+        int n = s.length();
+        int j = n - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                int c=i;
+                int d=j;
+                i=c+1;
+                while(i<j){
+                    if(s.charAt(i)!=s.charAt(j)) break;
+                    i++;
+                    j--;
                 }
-
-                if(l >= r) return true;
-
-                l = i;
-                r = j - 1;
-
-                while(l < r && s.charAt(l) == s.charAt(r)) {
-                    l++;
-                    r--;
+                if(i>=j) return true;
+                i=c;
+                j=d-1;
+                while(i<j){
+                    if(s.charAt(i)!=s.charAt(j)) break;
+                    i++;
+                    j--;
                 }
-
-                return l >= r;
+                return (i>=j);
             }
-
             i++;
             j--;
         }
-
         return true;
     }
 }

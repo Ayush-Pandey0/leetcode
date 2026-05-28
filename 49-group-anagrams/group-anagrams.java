919a74a5758da8c6e2ca9f36385f870bf29bd430
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-05-28 12:27:23 +0530

    Time: 541 ms (5.05%) | Memory: 49.6 MB (72.56%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    public boolean isAnagram(String a, String b) {
+
+        if(a.length() != b.length()) {
+            return false;
+        }
+
+        int[] freq = new int[26];
+
+        for(int i = 0; i < a.length(); i++) {
+            freq[a.charAt(i) - 'a']++;
+            freq[b.charAt(i) - 'a']--;
+        }
+
+        for(int i : freq) {
+            if(i != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        List<List<String>> ans = new ArrayList<>();
+        boolean[] visited = new boolean[strs.length];
+
+        for(int i = 0; i < strs.length; i++) {
+
+            if(visited[i]) {
+                continue;
+            }
+
+            List<String> group = new ArrayList<>();
+            group.add(strs[i]);
+            visited[i] = true;
+
+            for(int j = i + 1; j < strs.length; j++) {
+
+                if(!visited[j] && isAnagram(strs[i], strs[j])) {
+
+                    group.add(strs[j]);
+                    visited[j] = true;
+                }
+            }
+
+            ans.add(group);
+        }
+
+        return ans;
+    }
+}

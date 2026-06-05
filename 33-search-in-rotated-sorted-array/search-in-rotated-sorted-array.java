9ca2712810bb75bfa1d9e02dbea991beffdac43a
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-06-05 08:50:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (99.94%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,22 +1,41 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int low=0;
-        int high=nums.length-1;
 
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target) return mid;
-            if(nums[low]<=nums[mid]){
-                if(target>=nums[low]&&target<=nums[mid]){
-                    high=mid-1;
-                }else{
-                    low=mid+1;
+    public int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }
+
+            // Left half sorted
+            if(nums[left] <= nums[mid]) {
+
+                if(target >= nums[left] &&
+                   target < nums[mid]) {
+
+                    right = mid - 1;
                 }
-            }else{
-                if(target>=nums[mid]&&target<=nums[high]){
-                    low=mid+1;
-                }else{
-                    high=mid-1;
+                else {
+                    left = mid + 1;
+                }
+            }
+
+            // Right half sorted
+            else {
+
+                if(target > nums[mid] &&
+                   target <= nums[right]) {
+
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
                 }
             }
         }

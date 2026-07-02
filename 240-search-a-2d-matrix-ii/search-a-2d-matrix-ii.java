3ff56eb76979310c3f55bd72fa82753bfe93cf33
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-07-02 16:31:28 +0530

    Time: 3 ms (88.98%) | Memory: 48.2 MB (44.78%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,15 +1,22 @@
 class Solution {
+    public boolean find(int[][] matrix, int target,int row,int col,int n,int m){
+        if(row>=n||col<0) return false;
+        if(matrix[row][col]==target) return true;
+        if(matrix[row][col]>target) return find(matrix,target,row,col-1,n,m);
+        else return find(matrix,target,row+1,col,n,m);
+    }
     public boolean searchMatrix(int[][] matrix, int target) {
         int n=matrix.length;
         int m=matrix[0].length-1;
         int row=0;
         int col=m;
-        while(row<n&&col>=0){
-            if(matrix[row][col]==target) return true;
-            if(matrix[row][col]>target) col--;
-            else row++;
-        }
-        
+        return  find(matrix,target,row,col,n,m);
+        // while(row<n&&col>=0){
+        //     if(matrix[row][col]==target) return true;
+        //     if(matrix[row][col]>target) col--;
+        //     else row++;
+        // }
+
         // for(int i=0;i<n;i++){
         //     int low=0;
         //     int high=m;
@@ -21,6 +28,6 @@ class Solution {
         //         }else low=mid+1;
         //     }
         // }
-        return false;
+        // return false;
     }
 }

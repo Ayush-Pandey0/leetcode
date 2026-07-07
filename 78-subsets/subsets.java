405411aeb6dd67d8ac4a725a42fcde06a9efa3d1
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-07-07 18:53:40 +0530

    Time: 1 ms (87.53%) | Memory: 44.5 MB (7.50%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,18 +1,18 @@
 class Solution {
-    public void find(int[]nums,List<List<Integer>> ans,List<Integer>curr,int i){
+    public void recurse(List<List<Integer>> ans, List<Integer> curr,int[] nums,int i){
         if(i==nums.length){
             ans.add(new ArrayList<>(curr));
             return;
         }
         curr.add(nums[i]);
-        find(nums,ans,curr,i+1);
+        recurse(ans,curr,nums,i+1);
         curr.remove(curr.size()-1);
-        find(nums,ans,curr,i+1);
+        recurse(ans,curr,nums,i+1);
     }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans=new ArrayList<>();
         List<Integer> curr=new ArrayList<>();
-        find(nums,ans,curr,0);
+        recurse(ans,curr,nums,0);
         return ans;
     }
 }

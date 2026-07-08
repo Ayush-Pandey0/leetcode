664b36565509db6828db04bd7c8e3aaee34e7469
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-07-08 15:24:34 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (79.21%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,14 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    TreeNode prev=null;
+    int ans=1;
+    void find(TreeNode root){
+        if(root==null){
+            return;
+        }
+        find(root.left);
+        if(prev!=null&&prev.val>=root.val) {
+            ans=0;
+            return;
+        }
+        prev=root;
+        find(root.right);
+
     }
-
-    public boolean helper(TreeNode node, long min, long max) {
-        if (node == null) return true;
-
-        if (node.val <= min || node.val >= max) return false;
-
-        return helper(node.left, min, node.val) &&
-               helper(node.right, node.val, max);
+    public boolean isValidBST(TreeNode root) {
+        find(root);
+        return (ans==1)?true:false;
     }
 }

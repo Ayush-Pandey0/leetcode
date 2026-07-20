d2150b9b5a3c219271821bdbc813fc4197e5f190
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-07-20 21:59:06 +0530

    Time: 3 ms (27.42%) | Memory: 47.6 MB (87.63%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,41 +1,37 @@
 class Solution {
-    public void solve(char[][] board) {
-        int n=board.length;
-        int m=board[0].length;
-        Queue<int[]>q=new LinkedList<>();
+    public void solve(char[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
         int visited[][]=new int[n][m];
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]=='O'&&(i==0||j==0||i==n-1||j==m-1)){
+        Queue<int[]>q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if((i==0||j==0||i==n-1||j==m-1)&&(grid[i][j]=='O')){
                     q.add(new int[]{i,j});
-                    visited[i][j]=1;
                 }
             }
         }
-        int dx[]={-1,1,0,0};
-        int dy[]={0,0,-1,1};
+        int dir[][]={{-1,0},{1,0},{0,-1},{0,1}};
         while(!q.isEmpty()){
-            int d[]=q.poll();
-            int i=d[0];
-            int j=d[1];
-
+            int no[]=q.poll();
+            int i=no[0];
+            int j=no[1];
+            visited[i][j]=1;
             for(int k=0;k<4;k++){
-                int x=dx[k]+i;
-                int y=dy[k]+j;
-                if(x>=0&&y>=0&&x<n&&y<m&&board[x][y]=='O'&&visited[x][y]==0){
-                    visited[x][y]=1;
+                int x=i+dir[k][0];
+                int y=j+dir[k][1];
+                if(x>=0&&y>=0&&x<n&&y<m&&grid[x][y]=='O'&&visited[x][y]==0){
                     q.add(new int[]{x,y});
+                    visited[x][y]=1;
                 }
             }
         }
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]=='O'&&visited[i][j]==0){
-                    board[i][j]='X';
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(visited[i][j]==0){
+                    grid[i][j]='X';
                 }
             }
         }
-        
-
     }
 }

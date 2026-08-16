392b54044dea13ce6ef77e561b201c336896c0ed
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-08-16 18:29:15 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (79.78%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
@@ -4,14 +4,17 @@ class Solution {
         int right=0;
         int n=s.length()-1;
         int count=0;
-        Map<Character,Integer> mp=new HashMap<>();
+        int []mp=new int[26];
+        // Map<Character,Integer> mp=new HashMap<>();
         int max=0;
         while(right<=n){
             char c=s.charAt(right);
-            mp.put(c,mp.getOrDefault(c,0)+1);
-            while(mp.get(c)>2){
+            mp[c-'a']++;
+            // mp.put(c,mp.getOrDefault(c,0)+1);
+            while(mp[c-'a']>2){
                 char leftside=s.charAt(left);
-                mp.put(leftside,mp.getOrDefault(leftside,0)-1);
+                // mp.put(leftside,mp.getOrDefault(leftside,0)-1);
+                mp[leftside-'a']--;
                 left++;
             }
             max=Math.max(max,right-left);

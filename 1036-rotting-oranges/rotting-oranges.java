8f5c86936dfabb86dec234745265cd1da75948dc
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-08-31 20:43:16 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (72.19%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,61 +1,55 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-
-        Queue<int[]> q = new LinkedList<>();
-        int fresh = 0;
-
-        // Store all initially rotten oranges and count fresh ones
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    q.offer(new int[]{i, j});
-                } else if (grid[i][j] == 1) {
-                    fresh++;
+        int n=grid.length;
+        int m=grid[0].length;
+        Queue<int[]>q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.add(new int[]{i,j});
                 }
             }
         }
-
-        // No fresh oranges
-        if (fresh == 0) {
-            return 0;
-        }
-
-        int[][] directions = {
-            {-1, 0},
-            {1, 0},
-            {0, -1},
-            {0, 1}
-        };
-
-        int minutes = 0;
-
+        int minutes=0;
+        int dx[]={-1,0,1,0};
+        int dy[]={0,-1,0,1};
         while (!q.isEmpty()) {
             int size = q.size();
-            boolean rotted = false;
+            boolean rotten = false;
 
-            for (int i = 0; i < size; i++) {
+            for (int k = 0; k < size; k++) {
                 int[] curr = q.poll();
 
-                for (int[] dir : directions) {
-                    int x = curr[0] + dir[0];
-                    int y = curr[1] + dir[1];
+                int a = curr[0];
+                int b = curr[1];
 
-                    if (x >= 0 && y >= 0 && x < n && y < m && grid[x][y] == 1) {
+                for (int i = 0; i < 4; i++) {
+                    int x = a + dx[i];
+                    int y = b + dy[i];
+
+                    if (x >= 0 && y >= 0 && x < n && y < m
+                            && grid[x][y] == 1) {
+
                         grid[x][y] = 2;
-                        fresh--;
-                        q.offer(new int[]{x, y});
-                        rotted = true;
+                        q.add(new int[]{x, y});
+                        rotten = true;
                     }
                 }
             }
 
-            if (rotted) {
+            if (rotten) {
                 minutes++;
             }
         }
 
-        return fresh == 0 ? minutes : -1;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+        return minutes;
     }
 }

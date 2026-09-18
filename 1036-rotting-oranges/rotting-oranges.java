0cb5cd542fa5979f3a6ad00250c8b3ca33f63502
AYUSH KUMAR PANDEY <150110911+Ayush-Pandey0@users.noreply.github.com>
2026-09-18 22:00:59 +0530

    Time: 2 ms (87.59%) | Memory: 44.1 MB (55.40%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,55 +1,46 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        Queue<int[]> q=new LinkedList<>();
         int n=grid.length;
         int m=grid[0].length;
-        Queue<int[]>q=new LinkedList<>();
+        int fresh=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.add(new int[]{i,j});
+                }else if(grid[i][j]==1){
+                    fresh++;
                 }
             }
         }
-        int minutes=0;
-        int dx[]={-1,0,1,0};
-        int dy[]={0,-1,0,1};
-        while (!q.isEmpty()) {
-            int size = q.size();
-            boolean rotten = false;
-
-            for (int k = 0; k < size; k++) {
-                int[] curr = q.poll();
-
-                int a = curr[0];
-                int b = curr[1];
-
-                for (int i = 0; i < 4; i++) {
-                    int x = a + dx[i];
-                    int y = b + dy[i];
-
-                    if (x >= 0 && y >= 0 && x < n && y < m
-                            && grid[x][y] == 1) {
-
-                        grid[x][y] = 2;
-                        q.add(new int[]{x, y});
-                        rotten = true;
+        
+        int dx[]={-1,1,0,0};
+        int dy[]={0,0,-1,1};
+        int count=0;
+        while(!q.isEmpty()){
+            int size=q.size();
+            boolean rotten=false;
+            for(int i=0;i<size;i++){
+                int node[]=q.poll();
+                int a=node[0];
+                int b=node[1];
+                for(int j=0;j<4;j++){
+                    int x=a+dx[j];
+                    int y=b+dy[j];
+                    if(x>=0&&y>=0&&x<n&&y<m&&grid[x][y]==1){
+                        grid[x][y]=2;
+                        q.add(new int[]{x,y});
+                        rotten=true;
+                        fresh--;
                     }
                 }
             }
-
-            if (rotten) {
-                minutes++;
+            if(rotten){
+                count++;
             }
         }
 
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    return -1;
-                }
-            }
-        }
-        return minutes;
+        return (fresh==0)?count:-1;
+
     }
 }
